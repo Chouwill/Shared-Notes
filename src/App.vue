46b90acd693bf13941a83708ec624dffe8a5760e
Chouwill <will92068@gmail.com>
2025-12-17 21:32:23 +0800

    feat(note-editor): extract Toast UI Editor into NoteEditor component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { onMounted, ref } from "vue";
+  import Editor from "@toast-ui/editor";
+  import "@toast-ui/editor/dist/toastui-editor.css"; // Editor's Style
+
+  const editorRef = ref(null);
+  const editor = ref(null);
+
+  onMounted(() => {
+    console.log(editorRef.value);
+    editor.value = new Editor({
+      // el: document.querySelector("#editor"),
+      el: editorRef.value,
+      height: "500px",
+      initialEditType: "markdown",
+      previewStyle: "vertical",
+    });
+    editor.value.getMarkdown();
+  });
+  </script>
+
+  <template>
+    <div ref="editorRef"></div>
+  </template>
+
+  <style scoped></style>
