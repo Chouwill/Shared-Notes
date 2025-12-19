38ea2242af746eabf6a23f90a102507c27f40bb5
Chouwill <will92068@gmail.com>
2025-12-19 16:07:56 +0800

    fix: body background not responding to theme switch

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -5,12 +5,14 @@ import NoteIde from '../markIde/NoteIde.vue'
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="w-full bg-base-100">
     <!-- Header -->
     <Header />
 
     <main class="flex-1 relative mt-[100px] px-6">
-      <div class="text-slate-600"><NoteIde /></div>
+      <div class="text-slate-600">
+        <!-- <NoteIde /> -->
+      </div>
     </main>
   </div>
 </template>
