0bf607ae509e2f4ef581901da14d54f76bc9cb21
Chouwill <will92068@gmail.com>
2025-12-19 14:34:29 +0800

    feat(header): add basic header layout and styles

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="w-full h-[100px] bg-blue-300 text-white flex justify-around items-center">
+    <div class="flex justify-center items-center">
+      <img class="w-[150px] h-[50px]" src="../assets/images/Shared-Notes.png" alt="" />
+    </div>
+    <div class="w-[260px] bg-amber-600 rounded-3xl border">
+      <div role="tablist " class="tabs tabs-box flex justify-around">
+        <a role="tab" class="tab">首頁</a>
+        <a role="tab" class="tab tab-active">公開筆記</a>
+        <a role="tab" class="tab">標籤</a>
+        <a role="tab" class="tab">關於</a>
+      </div>
+    </div>
+    <div class="flex gap-3">
+      <button class="btn btn-neutral rounded-3xl">登入</button>
+      <button class="btn btn-primary rounded-3xl">註冊</button>
+    </div>
+  </div>
+</template>
---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from '../Header.vue'
+
+import NoteIde from '../markIde/NoteIde.vue'
+</script>
+
+<template>
+  <div class="w-full">
+    <!-- Header -->
+    <Header />
+
+    <main class="flex-1 relative mt-[100px] px-6">
+      <div class="text-slate-600"><NoteIde /></div>
+    </main>
+  </div>
+</template>
