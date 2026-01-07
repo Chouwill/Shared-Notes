39722235a4e9113e5f6ad2e8800c7abf542bf757
Chouwill <will92068@gmail.com>
2026-01-07 19:43:48 +0800

    feat(ui): add more dropdown menu and align UI colors with DaisyUI themes

--- src/page/NoteSpace.vue
+++ src/page/NoteSpace.vue
@@ -28,7 +28,8 @@ const noteLists = ref<InoteLists[]>([
     folderId: '',
   },
 ])
-const folders = ref<Ifolders[]>([])
+const folders = ref<Ifolders[]>([]) //儲存所有的資料夾
+
 const favoriteNotes = ref<Ifolders[]>([])
 
 const selectedNote = ref<number[]>([])
@@ -38,6 +39,8 @@ const addFolderdialog = ref<null>(null)
 const folderName = ref<string>('請輸入資料夾名稱')
 
 const message = ref(false)
+
+const dropdown = ref<string | null>(null)
 
 function addFolderName() {
   console.log(folderName.value)
@@ -72,146 +75,138 @@ function addfavoriteNotes(item) {
 function closeDialog() {
   addFolderdialog.value.close()
 }
+
+function selectOption(id: string) {
+  // dropdown.value = dropdown.value === id ? null : id
+  if (dropdown.value === id) {
+    dropdown.value = null
+  } else {
+    dropdown.value = id
+  }
+}
 </script>
 
 <template>
-  <div class="md:w-full w-[85%] mx-auto py-8 flex border-2">
+  <div class="md:w-full w-[85%] mx-auto pb-8 flex border border-base-300">
     <div class="flex flex-col gap-10">
-      <div class="!border overflow-x-hidden w-56">
-        <div class="border p-3 bg-[#2D01A5]">
-          <button class="w-full h-[25px] flex flex-row items-center gap-3">
-            <i class="fa-solid fa-plus text-sm"></i>
-            <div class="flex flex-row text-base-200">建立我的筆記</div>
-          </button>
-        </div>
-        <details class="rounded-box">
-          <summary class="menu-title">我的筆記</summary>
-          <ul>
-            <li v-for="item in folders" :key="item.id" class="pl-3 text-base-200">
-              <div class="flex overflow-x-hidden items-center gap-3">
-                <!-- 文字區：只能吃剩餘空間，超過就裁 -->
-                <div class="flex-1 min-w-0 overflo-x-hidden truncate p-1ㄙㄩㄣ">
-                  <p class="text-black">{{ item.name }}</p>
-                </div>
-
-                <!-- 按鈕區：固定寬度，永不換行 -->
-                <button class="btn-reset w-[25px] h-[25px] shrink-0 rounded-full">
-                  <i class="fa-solid fa-pen text-base text-black"></i>
-                </button>
+      <div class="border border-base-300 overflow-x-hidden w-56">
+        <div class="border-b border-base-300 flex flex-col items-start gap-3 p-3">
+          <div class="card card-side w-full p-4 bg-base-100 shadow-sm">
+            <div class="avatar">
+              <div class="w-14 rounded-xl">
+                <img src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp" />
               </div>
-            </li>
-          </ul>
-        </details>
+            </div>
+            <div class="card-body"></div>
+          </div>
+
+          <label class="input">
+            <svg
+              class="h-[1em] text-base-content/60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" class="search grow" placeholder="Search" />
+          </label>
+        </div>
+
+        <div class="font-black text-base text-base-content p-3">資料夾</div>
+
+        <div class="flex flex-col justify-center items-center gap-2">
+          <div
+            v-for="item in folders"
+            :key="item.id"
+            class="bg-base-200 hover:bg-base-300 w-[90%] flex justify-around p-4 rounded-xl transition-colors"
+          >
+            <div class="folder overflow-hidden whitespace-nowrap w-[120px] text-base-content">
+              <div>{{ item.name }}</div>
+            </div>
+            <button class="btn-reset w-[25px] h-[25px] shrink-0 rounded-full">
+              <i class="fa-solid fa-pen text-base text-base-content/60 hover:text-base-content"></i>
+            </button>
+          </div>
+        </div>
       </div>
+
       <div>
-        <div class="menu bg-base-200 rounded-box w-56 flex flex-row items-center gap-2 pl-5">
-          <i class="fa-regular fa-bookmark text-sm"></i>
+        <div
+          class="menu bg-base-200 rounded-box w-56 flex flex-row items-center gap-2 pl-5 text-base-content"
+        >
+          <i class="fa-regular fa-bookmark text-sm text-base-content/60"></i>
           <div>收藏</div>
         </div>
       </div>
     </div>
 
-    <div class="overflow-x-auto">
-      <div class="!border w-[150px]">
-        <button
-          @click="addFolder"
-          class="btn-reset w-[75px] h-[25px] rounded-full"
-        >
-          <i class="fa-solid fa-plus text-base"></i>
-          <div>新增資料夾</div>
-        </button>
+    <div class="overflow-x-auto w-full">
+      <div class="flex gap-2 p-2 justify-end">
+        <div class="flex w-[280px] gap-2">
+          <button class="btn btn-info flex-1" @click="addFolder">
+            <i class="fa-solid fa-plus text-base"></i>
+            <div>新增筆記</div>
+          </button>
+          <button class="btn flex-1" @click="addFolder">
+            <i class="fa-solid fa-plus text-base"></i>
+            <div>新增資料夾</div>
+          </button>
+        </div>
       </div>
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" class="checkbox" />
-              </label>
-            </th>
-            <th>文章標題</th>
-            <th>更新時間</th>
-            <th>Favorite Color</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr v-for="item in noteLists" :key="item.id">
-            <th>
-              <label>
-                <input type="checkbox" class="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div class="flex items-center gap-3">
-                <div class="avatar">
-                  <div class="mask mask-squircle h-10 w-10">
-                    <img src="../assets/images/google-font-docs.svg" alt="" />
-                  </div>
-                </div>
-                <div>
-                  <div class="font-bold">{{ item.title }}</div>
-                  <div class="text-sm opacity-50">{{ item.title }}</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              <span class="badge badge-ghost badge-sm">{{ item.updateDate }}</span>
-            </td>
-            <td class="border-5">
-              <div class="flex">
-                <button class=" w-[25px] h-[25px] rounded-full">
-                  <i class="fa-solid fa-thumbtack text-base"></i>
-                </button>
-                <button class=" w-[25px] h-[25px] rounded-full">
-                  <i class="fa-solid fa-tag text-base"></i>
-                </button>
-                <button class=" w-[25px] h-[25px] rounded-full">
-                  <i class="fa-solid fa-pen text-base"></i>
-                </button>
 
-                <button class=" w-[25px] h-[25px] flex justify-center items-center rounded-full">
-                  <!-- <img
-                    class="w-[16px] h-[16px]"
-                    src="../assets/images/bookmark-regular-full.svg"
-                    alt=""
-                  /> -->
-                  <i class="fa-solid fa-bookmark"></i>
-                </button>
+      <div class="border border-base-300 flex gap-3 pl-8 flex-wrap justify-start items-center">
+        <div class="card bg-base-100 border border-base-300 w-[320px] shadow-sm" v-for="item in folders" :key="item.id">
+          <div class="card-body flex flex-row justify-between">
+            <div>
+              <h2 class="file-title font-black text-base text-base-content">
+                {{ item.name }}
+              </h2>
+              <p class="text-sm text-base-content/60">8 個項目</p>
+            </div>
 
-                <button
-                  @click="addfavoriteNotes(item)"
-                  class="btn-reset w-[32px] h-[32px] flex justify-center items-center  rounded-full relative z-10"
-                  style="min-width: 32px; min-height: 32px"
-                >
-                  <i class="fa-regular fa-bookmark"></i>
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-        <!-- foot -->
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Favorite Color</th>
-            <th></th>
-          </tr>
-        </tfoot>
-      </table>
+            <div class="flex flex-col justify-center items-end relative">
+              <button @click="selectOption(item.id)">
+                <img src="@/assets/images/icon-more.png" class="w-[25px] h-[25px]" alt="" />
+              </button>
+              <ul
+                class="menu bg-base-200 rounded-box w-36 absolute top-8 z-[5] text-base-content"
+                v-if="dropdown === item.id"
+              >
+                <li>
+                  <button>
+                    <i class="fa-solid fa-pen text-base-content/60"></i>
+                    編輯資料夾
+                  </button>
+                </li>
+                <li>
+                  <button class="hover:text-error">
+                    <i class="fa-solid fa-trash"></i>
+                    刪除資料夾
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <dialog ref="addFolderdialog" class="modal">
       <div
-        class="modal-box h-[200px] relative border-2 flex justify-center items-center gap-3 flex-col"
+        class="modal-box h-[200px] relative border border-base-300 flex justify-center items-center gap-3 flex-col"
       >
         <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 
-        <label>請輸入新增的資料夾名稱</label>
+        <label class="text-base-content">請輸入新增的資料夾名稱</label>
         <input v-model="folderName" type="text" placeholder="" class="input" />
 
         <div class="flex gap-3">
