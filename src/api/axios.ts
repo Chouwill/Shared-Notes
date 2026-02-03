7e5ba2f55fec82a682a14e91b447a0b986c5c723
Chouwill <will92068@gmail.com>
2026-02-03 15:48:56 +0800

    Remove unnecessary custom header from axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,7 +7,6 @@ const baseURL = import.meta.env.VITE_API_BASE_URL || ''
 const instance = axios.create({
   baseURL,
   timeout: 7000,
-  headers: { 'X-Custom-Header': 'foobar' },
 })
 
 export default instance
